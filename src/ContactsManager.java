a81b4af4abbd405f9779bdd2e718d026c39e1f78
jackhowa <>
2019-02-24 15:26:07 -0600

    Use private fields and have public actions for manager of contacts
    Signed-off-by: jackhowa <>

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,24 +1,29 @@
-public class ContactsManager {
-  // fields
-  Contact [] myFriends;
-  int friendsCount;
+/**
+ * @author jackhowa
+ */
+class ContactsManager {
+  private Contact [] myFriends;
+  private int friendsCount;
 
-
-  // constructor
+  /**
+   * Create contact manager object with default 500 contacts available
+   * Start with zero friends created
+   */
   ContactsManager() {
     friendsCount = 0;
     myFriends = new Contact[500];
   }
 
-  // methods
   void addContact(Contact contact) {
     myFriends[friendsCount] = contact;
+
+    // fiends count remains private and used for adding to contacts
     friendsCount++;
   }
 
   Contact searchContact(String searchName) {
     for (int i = 0; i < friendsCount; i++) {
-      if (myFriends[i].name.equals(searchName)) {
+      if (myFriends[i].getName().equals(searchName)) {
         return myFriends[i];
       }
     }
