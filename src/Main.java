bf616c525710ffaa814127d89440047251a5f8ba
jackhowa <>
2019-02-24 14:40:38 -0600

    Find phone number via contact manager
    Signed-off-by: jackhowa <>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
 public class Main {
   public static void main(String [] args) {
-    System.out.println("Yo world");
+    // Create the ContactsManager object
     ContactsManager contactsManager = new ContactsManager();
-    System.out.println("Friendos count");
-    System.out.println(contactsManager.myFriends.length);
+
+    // create Contact object called wife
+    Contact wife = new Contact();
+
+    // set the object's fields
+    wife.name = "Noname";
+    wife.phoneNumber = "1800202luna";
+
+    // add the Contact object to the field myFriends via addContact method
+    contactsManager.addContact(wife);
+
+    Contact dad = new Contact();
+    dad.name = "Noone";
+    dad.phoneNumber = "8008335555";
+
+    contactsManager.addContact(dad);
+
+    Contact targetFriend = contactsManager.searchContact("Noname");
+
+    System.out.println(targetFriend.phoneNumber);
   }
 }
