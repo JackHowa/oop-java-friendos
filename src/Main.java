56debe44c1c1300f257ee85a99f69a72b5b605ba
jackhowa <>
2019-02-24 15:26:36 -0600

    Use constructor rather than unsafe directly modifying object after created
    Signed-off-by: jackhowa <>

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
+/**
+ * @author jackhowa
+ */
 public class Main {
   public static void main(String [] args) {
     // Create the ContactsManager object
     ContactsManager contactsManager = new ContactsManager();
 
     // create Contact object called wife
-    Contact wife = new Contact();
-
-    // set the object's fields
-    wife.name = "Noname";
-    wife.phoneNumber = "1800202luna";
+    Contact wife = new Contact(
+        "Noname",
+        "1800202luna"
+    );
 
     // add the Contact object to the field myFriends via addContact method
     contactsManager.addContact(wife);
 
-    Contact dad = new Contact();
-    dad.name = "Noone";
-    dad.phoneNumber = "8008335555";
+    Contact dad = new Contact(
+        "Noone",
+        "8008335555"
+    );
 
     contactsManager.addContact(dad);
 
     Contact targetFriend = contactsManager.searchContact("Noname");
 
-    System.out.println(targetFriend.phoneNumber);
+    System.out.println(targetFriend.getPhoneNumber());
   }
 }
